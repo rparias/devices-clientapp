e8a25d5965664ba92429821f4519b3873ff23e5e
Ronald Arias <ariasron@hotmail.com>
2021-12-05 08:21:02 -0500

    Finished styles for dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import DashboardContainer from './styles'
 import Button from '../../components/elements/Button'
+import Dropdown from '../../components/elements/Dropdown'
 import Table from '../../components/patterns/Table'
 import ModalDialog from '../../components/patterns/ModalDialog'
+import { DashboardContainer, HeaderContainer } from './styles'
+import { filterDeviceTypeList, filterSortByList } from '../../data/dropdown_options'
 
 const Dashboard = () => {
   const [openDialog, setOpenDialog] = useState(false)
@@ -16,12 +18,22 @@ const Dashboard = () => {
   }
 
   return (
-    <DashboardContainer>
-      <h1 className="text-white ff-sans-normal">Devices Client App</h1>
-      <Button onClick={handleOnOpenDialog}>Add Device</Button>
-      <ModalDialog open={openDialog} handleOnClose={handleOnCloseDialog} />
-      <Table />
-    </DashboardContainer>
+    <>
+      <HeaderContainer className="bg-beige flex">
+        <h1 className="text-dark ff-sans-normal fs-700 uppercase">Dashboard</h1>
+      </HeaderContainer>
+      <DashboardContainer>
+        <div className="flex space-between options">
+          <div className="flex options">
+            <Dropdown label="Device Type:" name="type" optionsDropdown={filterDeviceTypeList} />
+            <Dropdown label="Sort by:" name="sort" optionsDropdown={filterSortByList} />
+          </div>
+          <Button onClick={handleOnOpenDialog}>Add Device</Button>
+        </div>
+        <ModalDialog open={openDialog} handleOnClose={handleOnCloseDialog} />
+        <Table />
+      </DashboardContainer>
+    </>
   )
 }
 
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,6 +2,17 @@ import styled from 'styled-components'
 
 const DashboardContainer = styled.main`
   padding: 2rem;
+
+  .options {
+    @media screen and (max-width: 700px) {
+      flex-direction: column;
+    }
+  }
 `
 
-export default DashboardContainer
+const HeaderContainer = styled.header`
+  justify-content: center;
+  padding: 1rem 0;
+`
+
+export { DashboardContainer, HeaderContainer }
